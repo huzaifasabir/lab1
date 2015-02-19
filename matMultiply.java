d66bbcf47f6f7d77ecec547bf8c941f20bc30cb0
huzaifasabir <huzaifa249@gmail.com>
2015-02-20 00:16:09 +0500

    Update matMultiply.java

--- matMultiply.java
+++ matMultiply.java
@@ -7,93 +7,65 @@ import java.util.List;
 
 import org.junit.Test;
 
-public class matMulAdd {
-
-	
+public class matMultiply {
 
 	@Test
-	public void testMultiplyAndAdd() {
-		
-		int  l, d;
+	//test for multiplication of two matrices
+	public void testMultiply() {
+		int  l, d, k;
 		mat m = new mat();
-		   List<int[][]> list2d = new ArrayList<int[][]>(); 
-		 //reading file for populating inputs
-		   list2d =  m.readfile();
-		 if(list2d.size()<3){
-			System.out.println("Error occured there are not enough matrices to apply operations upon"); 
+		   List<int[][]> list2d = new ArrayList<int[][]>();
+		   //reading file for populating inputs
+		 list2d =  m.readfile();
+		 if(list2d.size()<2){
+			System.out.println("Error occured there are not enough matrices to be multiplied"); 
 		 }else{
-			//declaring 2d arrays and knowing there dimensions  
-			 int mat[][]=list2d.get(0);
-          int r = mat.length;
-          int c = mat[0].length;
-          int mat1[][]=list2d.get(1);
-          int rt1 = mat1.length;
-          int ct1 = mat1[0].length;
-          int mat2[][]=list2d.get(2);
-          int rt2 = mat2.length;
-          int ct2 = mat2[0].length;
-          int temp[][];//=list2d.get(2);
-          int result[][] = null;
-          //int rt3 = temp.length;
-          //int ct3 = temp[0].length;
-          list2d.size();
-          //intlist2d.get(0);
-          temp = m.multiply(list2d.get(0),list2d.get(1),r,rt1,c,ct1);
-          if(temp != null){
-        	  
-        	  result = m.add(temp,mat2,r,rt2,ct1,ct2);
-        	  
-          }
-        //printing matrices
-          System.out.println("Selected matrices");
-          System.out.println("Matrix 1");
-          
-          for ( l = 0 ; l < r ; l++ )
+			 //declaring 2d arrays and knowing there dimensions 
+			 int op[][]=list2d.get(0);
+             int r = op.length;
+             int c = op[0].length;
+             int op1[][]=list2d.get(1);
+             int rt1 = op1.length;
+             int ct1 = op1[0].length;
+             list2d.size();
+             int result[][];
+             //calling multiply functions
+             result = m.multiply(list2d.get(0),list2d.get(1),r,rt1,c,ct1);
+             
+             //printing matrices
+             System.out.println("Selected matrices");
+             
+             System.out.println("Matrix 1");
+             
+             for ( l = 0 ; l < r ; l++ )
 	         {
 	            for ( d = 0 ; d < c ; d++ )
-	               System.out.print(mat[l][d]+"\t");
+	               System.out.print(op[l][d]+"\t");
 	 
 	            System.out.print("\n");
 	         }
-          System.out.println("Matrix 2");
-          for ( l = 0 ; l < rt1 ; l++ )
+             System.out.println("Matrix 2");
+             for ( l = 0 ; l < rt1 ; l++ )
 	         {
 	            for ( d = 0 ; d < ct1 ; d++ )
-	               System.out.print(mat1[l][d]+"\t");
+	               System.out.print(op1[l][d]+"\t");
 	 
 	            System.out.print("\n");
-	         } 
-          
-          System.out.println("Matrix 3");
-          for ( l = 0 ; l < rt2 ; l++ )
+	         }
+             //printing result
+             if(result != null){
+             System.out.println("Product of selected matrices:");
+        	 
+	         for ( l = 0 ; l < r ; l++ )
 	         {
-	            for ( d = 0 ; d < ct2 ; d++ )
-	               System.out.print(mat2[l][d]+"\t");
-	 
-	            System.out.print("\n");
-	         } 
-        //printing result
-		 if(result != null){
-          System.out.println("Result of multiplication of two matrices then addtion of third to result");
-          for ( l = 0 ; l < r ; l++ )
-	         {
-	            for ( d = 0 ; d < c ; d++ )
+	            for ( d = 0 ; d < ct1 ; d++ )
 	               System.out.print(result[l][d]+"\t");
 	 
 	            System.out.print("\n");
-	         } 
-          
+	         }
+             }
+	         System.out.println("Test completed");
 		 }
-		 }
-		 System.out.println("Test completed");
-		
 	}
-		
-		
-		
-		
-		
-		
+
 }
-
-
