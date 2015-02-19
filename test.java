0a5528b6ff409e46e035b40cb47e2f27a95f9959
huzaifasabir <huzaifa249@gmail.com>
2015-02-20 00:17:27 +0500

    Create test.java

---
+++ test.java
@@ -0,0 +1,78 @@
+package matrix;
+
+
+import static org.junit.Assert.*;
+
+import java.util.*;
+
+import org.junit.Test;
+
+public class test {
+	
+
+		
+		@Test
+		//test for multiplication of two matrices
+		public void testSubtract() {
+			int  l, d, k;
+			mat m = new mat();
+			   List<int[][]> list2d = new ArrayList<int[][]>();
+			   //reading file for populating inputs
+			 list2d =  m.readfile();
+			 if(list2d.size()<2){
+				System.out.println("Error occured there are not enough matrices to be multiplied"); 
+			 }else{
+				 //declaring 2d arrays and knowing there dimensions 
+				 int op[][]=list2d.get(0);
+	             int r = op.length;
+	             int c = op[0].length;
+	             int op1[][]=list2d.get(1);
+	             int rt1 = op1.length;
+	             int ct1 = op1[0].length;
+	             list2d.size();
+	             int result[][];
+	             //calling subtract function
+	             result = m.subtract(list2d.get(0),list2d.get(1),r,rt1,c,ct1);
+	             
+	             //printing matrices
+	             System.out.println("Selected matrices");
+	             
+	             System.out.println("Matrix 1");
+	             
+	             for ( l = 0 ; l < r ; l++ )
+		         {
+		            for ( d = 0 ; d < c ; d++ )
+		               System.out.print(op[l][d]+"\t");
+		 
+		            System.out.print("\n");
+		         }
+	             System.out.println("Matrix 2");
+	             for ( l = 0 ; l < rt1 ; l++ )
+		         {
+		            for ( d = 0 ; d < ct1 ; d++ )
+		               System.out.print(op1[l][d]+"\t");
+		 
+		            System.out.print("\n");
+		         }
+	             //printing result
+	             if(result != null){
+	             System.out.println("Result of Subtraction:");
+	        	 
+		         for ( l = 0 ; l < r ; l++ )
+		         {
+		            for ( d = 0 ; d < ct1 ; d++ )
+		               System.out.print(result[l][d]+"\t");
+		 
+		            System.out.print("\n");
+		         }
+	             }
+		         System.out.println("Test completed");
+			 }
+		
+		
+	}
+	
+	
+	
+
+}
